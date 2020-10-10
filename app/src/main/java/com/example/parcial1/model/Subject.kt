f37501e425af000a336c3623cba8e9128c9dc4a9
calvoeduer <calvoeduer@gmail.com>
2020-10-09 20:23:57 -0500

    feat: creación paquetes y fragmentos

---
+++ app/src/main/java/com/example/parcial1/model/Subject.kt
@@ -0,0 +1,5 @@
+package com.example.parcial1.model
+
+class Subject(val code: String, val name: String, val credits: Int) {
+
+}
---
+++ app/src/main/java/com/example/parcial1/view/adapter/SubjectListener.kt
@@ -0,0 +1,4 @@
+package com.example.parcial1.view.adapter
+
+interface SubjectListener {
+}
